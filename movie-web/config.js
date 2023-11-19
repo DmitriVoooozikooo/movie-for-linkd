1bf940a5ea3cc3160d65278346180b811cfdb2e6
DmitriVoooozikooo <88106879+DmitriVoooozikooo@users.noreply.github.com>
2023-11-18 19:59:30 -0500

    Update config.js

--- movie-web/config.js
+++ movie-web/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "movie.d-tsarddakas.workers.dev",
+  VITE_CORS_PROXY_URL: "https://movie.d-tsarddakas.workers.dev",
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDVlYjY0ZmRjMDdkNzIxMWMyMTU4NWFhMzQ4MTI1MyIsInN1YiI6IjY1NTk1M2JiZWE4NGM3MTA5MTBkOTBkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TeQhaMdHJFNx_6MEKJJJM7VzDdwnP90E0gsDLp10XdQ"
 };
